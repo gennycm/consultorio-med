cda7beb68304f89480aa74f5a504c9ba802fd86d
Genny Centeno <gennycm13@gmail.com>
2020-03-10 20:29:10 -0600

    UI Improvements

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,90 +33,27 @@
          <!-- Divider -->
          <hr class="sidebar-divider my-0">
          <!-- Nav Item - Dashboard -->
-         <li class="nav-item active">
-            <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
+         <li class="nav-item {{ Request::is('home') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ url('/home') }}">
+            <i class="fas fa-fw fa-home"></i>
+            <span>Inicio</span></a>
          </li>
          <!-- Divider -->
          <hr class="sidebar-divider">
          <!-- Heading -->
          <div class="sidebar-heading">
-            Interface
+            Menú
          </div>
          <!-- Nav Item - Pages Collapse Menu -->
-         <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>Components</span>
-            </a>
-            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-               <div class="bg-white py-2 collapse-inner rounded">
-                  <h6 class="collapse-header">Custom Components:</h6>
-                  <a class="collapse-item" href="buttons.html">Buttons</a>
-                  <a class="collapse-item" href="cards.html">Cards</a>
-               </div>
-            </div>
+         <li class="nav-item {{ Request::is('users') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ url('/users') }}">
+            <i class="fas fa-fw fa-user"></i>
+            <span>Usuarios</span></a>
          </li>
-         <!-- Nav Item - Utilities Collapse Menu -->
-         <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Utilities</span>
-            </a>
-            <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-               <div class="bg-white py-2 collapse-inner rounded">
-                  <h6 class="collapse-header">Custom Utilities:</h6>
-                  <a class="collapse-item" href="utilities-color.html">Colors</a>
-                  <a class="collapse-item" href="utilities-border.html">Borders</a>
-                  <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                  <a class="collapse-item" href="utilities-other.html">Other</a>
-               </div>
-            </div>
-         </li>
-         <!-- Divider -->
-         <hr class="sidebar-divider">
-         <!-- Heading -->
-         <div class="sidebar-heading">
-            Addons
-         </div>
-         <!-- Nav Item - Pages Collapse Menu -->
-         <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-            <i class="fas fa-fw fa-folder"></i>
-            <span>Pages</span>
-            </a>
-            <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-               <div class="bg-white py-2 collapse-inner rounded">
-                  <h6 class="collapse-header">Login Screens:</h6>
-                  <a class="collapse-item" href="login.html">Login</a>
-                  <a class="collapse-item" href="register.html">Register</a>
-                  <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                  <div class="collapse-divider"></div>
-                  <h6 class="collapse-header">Other Pages:</h6>
-                  <a class="collapse-item" href="404.html">404 Page</a>
-                  <a class="collapse-item" href="blank.html">Blank Page</a>
-               </div>
-            </div>
-         </li>
-         <!-- Nav Item - Charts -->
-         <li class="nav-item">
-            <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>Charts</span></a>
-         </li>
-         <!-- Nav Item - Tables -->
-         <li class="nav-item">
-            <a class="nav-link" href="tables.html">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Tables</span></a>
-         </li>
+
          <!-- Divider -->
          <hr class="sidebar-divider d-none d-md-block">
-         <!-- Sidebar Toggler (Sidebar) -->
-         <div class="text-center d-none d-md-inline">
-            <button class="rounded-circle border-0" id="sidebarToggle"></button>
-         </div>
+
       </ul>
       <!-- End of Sidebar -->
       <!-- Content Wrapper -->
@@ -138,15 +75,11 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                           
                         @else
                             <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"> {{ Auth::user()->name }} <span class="caret"></span></span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600"> {{ Auth::user()->name }} </span>
                             </a>
 
                                 <!-- Dropdown - User Information -->
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,45 +1,64 @@
 <!-- index.blade.php -->
-
 @extends('layouts.app')
-
 @section('content')
 <style>
-  .uper {
-    margin-top: 40px;
-  }
+   .uper {
+   margin-top: 40px;
+   }
 </style>
-<div class="uper">
-  @if(session()->get('success'))
-    <div class="alert alert-success">
-      {{ session()->get('success') }}  
-    </div><br />
-  @endif
-  <table class="table table-striped">
-    <thead>
-        <tr>
-          <td>ID</td>
-          <td>Name</td>
-          <td>Email</td>
-          <td colspan="2">Action</td>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($users as $user)
-        <tr>
-            <td>{{$user->id}}</td>
-            <td>{{$user->name}}</td>
-            <td>{{$user->email}}</td>
-            <td><a href="{{ route('users.edit',$user->id)}}" class="btn btn-primary">Edit</a></td>
-            <td>
-                <form action="{{ route('users.destroy', $user->id)}}" method="post">
-                  @csrf
-                  @method('DELETE')
-                  <button class="btn btn-danger" type="submit">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-  </table>
-<div>
+<div class="container-fluid">
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+   <h1 class="h3 mb-0 text-gray-800">Usuarios</h1>
+</div>
+<!-- Content Row -->
+<div class="row">
+   <div class="col-md-12">
+      @if (session('status'))
+      <div class="alert alert-success" role="alert">
+         {{ session('status') }}
+      </div>
+      @endif
+   </div>
+</div>
+<div class="row">
+<div class="col-md-12">
+   <div class="uper">
+      @if(session()->get('success'))
+      <div class="alert alert-success">
+         {{ session()->get('success') }}  
+      </div>
+      <br />
+      @endif
+      <table class="table table-striped">
+         <thead>
+            <tr>
+               <td>ID</td>
+               <td>Name</td>
+               <td>Email</td>
+               <td colspan="2">Action</td>
+            </tr>
+         </thead>
+         <tbody>
+            @foreach($users as $user)
+            <tr>
+               <td>{{$user->id}}</td>
+               <td>{{$user->name}}</td>
+               <td>{{$user->email}}</td>
+               <td><a href="{{ route('users.edit',$user->id)}}" class="btn btn-primary">Edit</a></td>
+               <td>
+                  <form action="{{ route('users.destroy', $user->id)}}" method="post">
+                     @csrf
+                     @method('DELETE')
+                     <button class="btn btn-danger" type="submit">Delete</button>
+                  </form>
+               </td>
+            </tr>
+            @endforeach
+         </tbody>
+      </table>
+      <div>
+      </div>
+   </div>
+</div>
 @endsection
